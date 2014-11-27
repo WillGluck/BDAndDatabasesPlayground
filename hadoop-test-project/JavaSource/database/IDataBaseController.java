e2f659ff869edcbfb9b4edab7c1cc4c8f4717a4e
Will Glück <bigwillgluck@gmail.com>
2014-11-27 13:12:03 -0200

    Commit da primeira parte do projeto

---
+++ hadoop-test-project/JavaSource/database/IDataBaseController.java
@@ -0,0 +1,11 @@
+package database;
+
+/**
+ * Interface que define os comandos de acesso ao banco de dados.
+ * @author Will Glück
+ * @created 27/11/2014
+ *
+ */
+public interface IDataBaseController {
+
+}
---
+++ hadoop-test-project/JavaSource/mbeans/MainPageBean.java
@@ -0,0 +1,14 @@
+package mbeans;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+
+@ManagedBean(name="mainPage")
+@RequestScoped
+public class MainPageBean {
+
+	public MainPageBean() {
+		
+	}
+	
+}
