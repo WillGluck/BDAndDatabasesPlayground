0f5f428e1581f82b250005ebedc2939b57c3a01c
Will Glück <bigwillgluck@gmail.com>
2014-11-28 22:49:15 -0200

    Tuto

--- hadoop-test-project/JavaSource/database/HbaseDataBasecontroller.java
+++ hadoop-test-project/JavaSource/database/HbaseDataBasecontroller.java
@@ -1,9 +1,17 @@
 package database;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import models.Message;
 import models.User;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.client.HTable;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.util.Bytes;
 
 /**
  * Classe responsável pelo acesso ao banco via HBASE.
@@ -12,22 +20,37 @@ import models.User;
  * @created 20/11/2014
  */
 public class HbaseDataBasecontroller implements IDataBaseController {
+	
+	HBaseConfiguration hc;
+	org.apache.hadoop.conf.Configuration config;
+	HTable table;
+	
+	public HbaseDataBasecontroller() {
+		 this.hc = new HBaseConfiguration(new Configuration());
+		 this.config = HBaseConfiguration.create();
+		 try {
+			this.table = new HTable(config, "message");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
 	@Override
-	public void insertMessage(Message message) {
-		// TODO Auto-generated method stub
-		
+	public void insertMessage(Message message) {		
+		Put p = new Put(Bytes.toBytes(message.getUserTo().getCode()));
 	}
 
 	@Override
 	public List<Message> fetchLastMessages(int amount, User user, User userFrom) {
-		return null;		
+		
+		
+		return new ArrayList<Message>();		
 	}
 
 	@Override
 	public List<User> fetchUsers() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return new ArrayList<User>();
 	}
 
 }
